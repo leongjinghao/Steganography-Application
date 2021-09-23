a4f4f0bf9e44e5e9e5ab26619ac0981aa024d1e5
Clement Ong <2001298@sit.singaporetech.edu.sg>
2021-09-23 13:33:45 +0800

    Update gui.py

--- project/gui.py
+++ project/gui.py
@@ -1,1 +1,26 @@
-#file to initialize and maintain the GUI
+from tkinter import *
+from tkinter import ttk
+
+class Application (ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = ttk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = ttk.Button(self, text="QUIT", fg="red",
+                               command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = ttk.Tk()
+app = Application(master=root)
+app.mainloop()
