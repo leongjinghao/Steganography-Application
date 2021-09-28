9c426f249979fc4c6cef722aba1d987bbdcda6f9
CzarCerro <76830219+CzarCerro@users.noreply.github.com>
2021-09-29 01:18:05 +0800

    Update Stegasaurus(GUI).py

--- project/Stegasaurus(GUI).py
+++ project/Stegasaurus(GUI).py
@@ -17,7 +17,7 @@ class GUI:
         mainGUI.geometry("1100x700+100+50")  # Sets window size
         mainGUI.configure(bg=self.background) #Sets GUI background color
         mainGUI.resizable(width=True, height=True) #Disallows user from resizing the window.
-        self.mainMenu(mainGUI)
+        self.viewPage(mainGUI)
         mainGUI.mainloop()  # Starts up4 and runs the GUI until user exits
 
     def mainMenu(self, mainGUI):
@@ -34,10 +34,7 @@ class GUI:
         Radiobutton(mainMenuFrame, text='Payload', variable=uploadValue, value='1', background=self.background).place(relx=0.5, rely=0.2, relheight=0.05, relwidth=0.1)
         Radiobutton(mainMenuFrame, text='Cover', variable=uploadValue, value='2', background=self.background).place(relx=0.6, rely=0.2, relheight=0.05, relwidth=0.1)
 
-        #Upload button
-        uploadButton = Button(mainMenuFrame, text="Upload", command=lambda: [self.uploadFile(uploadValue.get())])
-        uploadButton.config(font=("Arial", 15))
-        uploadButton.place(relx=0.35, rely=0.2, relheight=0.05, relwidth=0.1)
+
 
         #Delete Radio button
         deleteValue = StringVar(mainMenuFrame, "1")
@@ -45,27 +42,35 @@ class GUI:
         Radiobutton(mainMenuFrame, text='Cover', variable=deleteValue, value='2', background=self.background).place(relx=0.6, rely=0.35, relheight=0.05, relwidth=0.1)
         Radiobutton(mainMenuFrame, text='All', variable=deleteValue, value='3', background=self.background).place(relx=0.7, rely=0.35, relheight=0.05, relwidth=0.1)
 
-        #Delete button
-        deleteButton = Button(mainMenuFrame, text="Delete", command=lambda: [self.deleteFile(deleteValue.get())])
-        deleteButton.config(font=("Arial", 15))
-        deleteButton.place(relx=0.35, rely=0.35, relheight=0.05, relwidth=0.1)
+
 
         #View button
         viewButton = Button(mainMenuFrame, text="View", command=lambda: [mainMenuFrame.place_forget(), self.viewPage(mainGUI)])
         viewButton.config(font=("Arial", 15))
         viewButton.place(relx=0.35, rely=0.5, relheight=0.05, relwidth=0.1)
 
-        #Hide button
-        hideButton = Button(mainMenuFrame, text="Hide payload",  command=lambda: [print(uploadValue.get(),deleteValue.get(),viewValue.get())])
-        hideButton.config(font=("Arial", 15))
-        hideButton.place(relx=0.2, rely=0.9, relheight=0.1, relwidth=0.2)
 
-        #Extract button
-        extractButton = Button(mainMenuFrame, text="Extract payload") #command=lambda: [self.crawlerPage(mainGUI), mainMenuFrame.place_forget()])
-        extractButton.config(font=("Arial", 15))
-        extractButton.place(relx=0.6, rely=0.9, relheight=0.1, relwidth=0.2)
 
         #-----------------------Stats interface-------------------------
+
+    def uploadPage(self, mainGUI):
+        uploadFrame = Frame(mainGUI, bg=self.background)
+        uploadFrame.place(relx=0, rely=0, relheight=1, relwidth=1)
+
+        #Payload button
+        payloadButton = Button(uploadFrame, text="Payload", command=lambda: [uploadFrame.place_forget(), self.uploadFile(1), self.viewPage(mainGUI)])
+        payloadButton.config(font=("Arial", 25))
+        payloadButton.place(relx=0.45, rely=0.35, relheight=0.08, relwidth=0.15)
+
+        #Cover button
+        coverButton = Button(uploadFrame, text="Cover", command=lambda: [uploadFrame.place_forget(), self.uploadFile(2), self.viewPage(mainGUI)])
+        coverButton.config(font=("Arial", 25))
+        coverButton.place(relx=0.45, rely=0.55, relheight=0.08, relwidth=0.15)
+
+        #Back button
+        coverButton = Button(uploadFrame, text="Back", command=lambda: [uploadFrame.place_forget(), self.viewPage(mainGUI)])
+        coverButton.config(font=("Arial", 25))
+        coverButton.place(relx=0.45, rely=0.75, relheight=0.08, relwidth=0.15)
 
     def uploadFile(self, uploadValue):
         filelocation = askopenfilename()
@@ -121,7 +126,7 @@ class GUI:
 
 
         title.config(font=("MS Sans Serif", 30))
-        title.place(relx=0, rely=0, relheight=0.1, relwidth=0.5)
+        title.place(relx=0, rely=0, relheight=0.1, relwidth=0.25)
 
         title = Label(viewFrame,
                                   text="Cover files",
@@ -129,7 +134,15 @@ class GUI:
 
 
         title.config(font=("MS Sans Serif", 30))
-        title.place(relx=0.5, rely=0, relheight=0.1, relwidth=0.5)
+        title.place(relx=0.35, rely=0, relheight=0.1, relwidth=0.25)
+
+        title = Label(viewFrame,
+                                  text="Result files",
+                                  bg=self.background)
+
+
+        title.config(font=("MS Sans Serif", 30))
+        title.place(relx=0.7, rely=0, relheight=0.1, relwidth=0.25)
 
 
 
@@ -140,16 +153,28 @@ class GUI:
 
         payloadList.column(item_column[0], anchor="w")
         payloadList.column(item_column[1], anchor="n", width=10)
-        payloadList.place(relx=0,rely=0.1, relheight=0.5, relwidth=0.5)
+        payloadList.place(relx=0,rely=0.1, relheight=0.5, relwidth=0.3333)
 
-        item_column = ('File Name','File Type')
+        #item_column = ('File Name','File Type')
         coverList = ttk.Treeview(viewFrame,columns=item_column,show='headings')
         for i in range(len(item_column)):
             coverList.heading(item_column[i], text=item_column[i])
 
         coverList.column(item_column[0], anchor="w")
         coverList.column(item_column[1], anchor="n", width=10)
-        coverList.place(relx=0.5,rely=0.1, relheight=0.5, relwidth=0.5)
+        coverList.place(relx=0.33333,rely=0.1, relheight=0.5, relwidth=0.3333)
+
+
+        resultList = ttk.Treeview(viewFrame,columns=item_column,show='headings')
+        for i in range(len(item_column)):
+            resultList.heading(item_column[i], text=item_column[i])
+
+        if len(payloadList.selection()) > 0:
+            payloadList.selection_remove(payloadList.selection()[0])
+
+        resultList.column(item_column[0], anchor="w")
+        resultList.column(item_column[1], anchor="n", width=10)
+        resultList.place(relx=0.66666,rely=0.1, relheight=0.5, relwidth=0.3333)
 
         payloadFiles = os.listdir('./payload')
 
@@ -168,13 +193,43 @@ class GUI:
             coverList.insert("", 0, values=(coverFiles[i]))
 
 
-        backButton = Button(viewFrame, text="Back", command=lambda: [viewFrame.place_forget(), self.mainMenu(mainGUI)])
-        backButton.config(font=("Arial", 30))
-        backButton.place(relx=0.05, rely=0.85, relheight=0.1, relwidth=0.2)
+
 
         viewPayloadHexButton = Button(viewFrame, text="View payload data", command=lambda: [self.retrieveBinaries(payloadList.item(payloadList.selection())['values'], "payload")])
         viewPayloadHexButton.config(font=("Arial", 11))
-        viewPayloadHexButton.place(relx=0.05, rely=0.65, relheight=0.1, relwidth=0.15)
+        viewPayloadHexButton.place(relx=0.05, rely=0.65, relheight=0.05, relwidth=0.15)
+
+        #Hide button
+        hideButton = Button(viewFrame, text="Hide payload",  command=lambda: [])
+        hideButton.config(font=("Arial", 15))
+        hideButton.place(relx=0.8, rely=0.85, relheight=0.05, relwidth=0.15)
+
+        #Extract button
+        extractButton = Button(viewFrame, text="Extract payload") #command=lambda: [self.crawlerPage(mainGUI), mainMenuFrame.place_forget()])
+        extractButton.config(font=("Arial", 15))
+        extractButton.place(relx=0.8, rely=0.9, relheight=0.05, relwidth=0.15)
+
+        viewPayloadHexButton = Button(viewFrame, text="Clear Selection", command=lambda: [self.clearSelection(payloadList,coverList,resultList)])
+        viewPayloadHexButton.config(font=("Arial", 11))
+        viewPayloadHexButton.place(relx=0.4, rely=0.65, relheight=0.05, relwidth=0.15)
+
+        #Upload button
+        uploadButton = Button(viewFrame, text="Upload", command=lambda: [viewFrame.place_forget(),self.uploadPage(mainGUI)])
+        uploadButton.config(font=("Arial", 15))
+        uploadButton.place(relx=0.05, rely=0.9, relheight=0.05, relwidth=0.2)
+
+        #Delete button
+        uploadButton = Button(viewFrame, text="Delete", command=lambda: [])
+        uploadButton.config(font=("Arial", 15))
+        uploadButton.place(relx=0.35, rely=0.9, relheight=0.05, relwidth=0.2)
+
+    def clearSelection(self,payloadList, coverList, resultList):
+        for item in payloadList.selection():
+            payloadList.selection_remove(item)
+        for item in coverList.selection():
+            coverList.selection_remove(item)
+        for item in resultList.selection():
+            resultList.selection_remove(item)
 
     def retrieveBinaries(self, fileSelection, payloadOrCover):
 
@@ -184,20 +239,10 @@ class GUI:
         file = file.convert('RGB')
         pixels = list(file.getdata())
         print(pixels)
-        #width, height = file.size
-        #pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
         print(pixels)
-        #ctypes.windll.user32.MessageBoxW(0, "Your text", fileSelection[0], 1)
 
-    # def image(self):
-    #     from PIL import Image
-    #     x = 0
-    #     y = 0
-    #     im = Image.open('test.tiff')  # Can be many different formats.
-    #     pixels = list(im.getdata())
-    #     width, height = im.size
-    #     pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-    #     #print(pixels)
+
+
 
 
 
