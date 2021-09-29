f4721e9c85e94bc703a1c24c191576b76d00573a
leongjinghao <73938217+leongjinghao@users.noreply.github.com>
2021-09-29 13:07:35 +0800

    implemented features
    - single bit replacement
    - multiple bit replacement

--- project/steganography.py
+++ project/steganography.py
@@ -17,7 +17,7 @@ def messageToBinary(message):
     #print(messageInBin)
     return messageInBin
 
-def hideData(image, message):
+def hideData(image, message, mode, bitSelect):
     # copy of image, the cover
     cover = image.copy()
 
@@ -30,27 +30,54 @@ def hideData(image, message):
     # index to keep track of the bit of payload to replace on the cover
     payloadIndex = 0
 
-    for rows in cover:              # traverse rows of image (height)
-        for pixel in rows:          # traverse each column of each row (pixel)
-            # extract binary B, G and R value from each pixel
-            b, g, r = messageToBinary(pixel[0]),    \
-                      messageToBinary(pixel[1]),    \
-                      messageToBinary(pixel[2])
-            # index of R is 2 (pixel[2] = r, pixel[1] = g, pixel[0] = b)
-            bgrIndex = 2
-            for colour in (r, g, b):
-                # if there are still bits left to be replaced
-                if payloadIndex < payloadLen:
-                    # retain value from bit 7 to 1, replace only bit 0 (LSB), convert value to binary using base 2
-                    pixel[bgrIndex] = int(colour[:-1] + payload[payloadIndex], 2)
-                    bgrIndex -= 1
-                    payloadIndex += 1
-                # else all bits of payload are replaced on the cover
-                else:
-                    cv2.imwrite('cover.png', cover)
-                    return
+    # validate if there are enough bit on cover for replacement (different value for each mode)
+    # number of possible single bit replacement on cover
+    if mode == 1:
+        # pixel count * 3 (RGB)
+        limit = image.shape[0] * image.shape[1] * 3
+        # initialise bit range as 1
+        bitRange = 1
+    # number of possible multiple bit replacement on cover
+    elif mode == 2:
+        # pixel count * 3 (RGB) * number of bits
+        limit = image.shape[0] * image.shape[1] * 3 * (bitSelect + 1)
+        # initialise bit range as bitSelect + 1 (start from bit 0)
+        bitRange = bitSelect + 1
+    if payloadLen > limit:
+        print("Error: Insufficient bit on cover!")
+        return
 
-def decoder(image):
+    for i in range(bitRange):
+        # if mode selected is single bit replacement, change bit to be replaced to the bit selected
+        if mode == 1:
+            i = bitSelect
+
+        for rows in cover:              # traverse rows of image (height)
+            for pixel in rows:          # traverse each column of each row (pixel)
+                # extract binary B, G and R value from each pixel
+                b, g, r = messageToBinary(pixel[0]),    \
+                          messageToBinary(pixel[1]),    \
+                          messageToBinary(pixel[2])
+                # index of R is 2 (pixel[2] = r, pixel[1] = g, pixel[0] = b)
+                bgrIndex = 2
+                for colour in (r, g, b):
+                    # if there are still bits left to be replaced
+                    if payloadIndex < payloadLen:
+                        # replace the current selected bit on bitRange
+                        # special case when current selected bit is bit 0
+                        if i == 0:
+                            pixel[bgrIndex] = int(colour[:-(i + 1)] + payload[payloadIndex], 2)
+                        else:
+                            pixel[bgrIndex] = int(colour[:-(i + 1)] + payload[payloadIndex] + colour[-i:], 2)
+                        # shift indexes accordingly
+                        bgrIndex -= 1
+                        payloadIndex += 1
+                    # else all bits of payload are replaced on the cover
+                    else:
+                        cv2.imwrite('cover.png', cover)
+                        return
+
+def decoder(image, bitSelect):
     sImage = cv2.imread(image)
     hiddenBinary = ''
     for rows in sImage:
@@ -58,9 +85,9 @@ def decoder(image):
             b, g, r = messageToBinary(pixel[0]), \
                       messageToBinary(pixel[1]), \
                       messageToBinary(pixel[2])
-            hiddenBinary += r[-1]
-            hiddenBinary += g[-1]
-            hiddenBinary += b[-1]
+            hiddenBinary += r[-(bitSelect + 1)]
+            hiddenBinary += g[-(bitSelect + 1)]
+            hiddenBinary += b[-(bitSelect + 1)]
 
     # group 8 bits into a byte
     hiddenByte = [hiddenBinary[i: i + 8] for i in range(0, len(hiddenBinary), 8)]
@@ -79,7 +106,9 @@ def decoder(image):
 if __name__ == '__main__':
     message = 'hello darkness my old friend'
     image = cv2.imread('Lenna.png')
-    hideData(image, message)
-    messageDecoded = decoder('cover.png')
+    mode = 1      # 1. change single bit, 2. multiple bit replacement
+    bitSelect = 3
+    hideData(image, message, mode, bitSelect)
+    messageDecoded = decoder('cover.png', bitSelect)
     print(messageDecoded)
 
